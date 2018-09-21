4f34ab8668ebe816681f977b1fb114f09e4540e4
fl0wjacky <flowjacky@gmail.com>
2018-09-21 16:18:14 +0800

    update for exa

--- ex.cs
+++ ex.cs
@@ -7,7 +7,7 @@ using System.Numerics;
 
 namespace Neo.SmartContract
 {
-    public class CT: Framework.SmartContract
+    public class EXV: Framework.SmartContract
     {
         private static readonly byte INVOCATION_TRANSACTION_TYPE = 0xd1;
         private const int LENGTH_OF_SCRIPTHASH = 20;
@@ -38,42 +38,44 @@ namespace Neo.SmartContract
                 if (itx.Script.Range(10, LENGTH_OF_SCRIPTHASH) != owner) return false;
                 return true;
             }
-            else if (166 == itx.Script.Length)
+            else if (165 == itx.Script.Length)
             {
                 if (price.AsBigInteger() <= 0) return false;
+                if (tokenSR.AsBigInteger() == tokenBR.AsBigInteger()) return false;
                 if (itx.Script[0] != 0x08) return false;
                 if (itx.Script[9] != 0x14) return false;
                 if (itx.Script[30] != 0x14) return false;
-                if (itx.Script.Range(51, 12) != new byte[] { 0x53, 0xc1, 0x08, 0x74, 0x72, 0x61, 0x6e, 0x73, 0x66, 0x65, 0x72, 0x67 }) return false;
-                if (itx.Script[83] != 0x08) return false;
-                if (itx.Script[92] != 0x14) return false;
-                if (itx.Script[113] != 0x14) return false;
-                if (itx.Script.Range(134, 12) != new byte[] { 0x53, 0xc1, 0x08, 0x74, 0x72, 0x61, 0x6e, 0x73, 0x66, 0x65, 0x72, 0x67 }) return false;
+                if (itx.Script[51] != 0x14) return false;
+                if (itx.Script[72] != 0x08) return false;
+                if (itx.Script[81] != 0x14) return false;
+                if (itx.Script[102] != 0x14) return false;
+                if (itx.Script[123] != 0x14) return false;
+                if (itx.Script.Range(144, 21) != new byte[] { 0x67, 0x63, 0x44, 0xbb, 0x3d, 0x1e, 0xeb, 0xc2, 0x54, 0x8c, 0x0f, 0xb8, 0x99, 0x6d, 0xef, 0x81, 0xe7, 0x8c, 0x3c, 0xaf  0x4a}) return false;
                 BigInteger amountS = 0;
                 BigInteger amountB = 0;
                 byte[] toS = null;
                 byte[] toB = null;
                 byte[] fromS = null;
                 byte[] fromB = null;
-                if (tokenSR == itx.Script.Range(63,20) && tokenBR == itx.Script.Range(146,20))
+                if (tokenSR == itx.Script.Range(52, LENGTH_OF_SCRIPTHASH) && tokenBR == itx.Script.Range(124, LENGTH_OF_SCRIPTHASH))
                 {
                     amountS = itx.Script.Range(1, LENGTH_OF_AMOUNT).AsBigInteger();
                     toS = itx.Script.Range(10, LENGTH_OF_SCRIPTHASH);
                     fromS = itx.Script.Range(31, LENGTH_OF_SCRIPTHASH);
                     
-                    amountB = itx.Script.Range(84, LENGTH_OF_AMOUNT).AsBigInteger();
-                    toB = itx.Script.Range(93, LENGTH_OF_SCRIPTHASH);
-                    fromB = itx.Script.Range(114, LENGTH_OF_SCRIPTHASH);
+                    amountB = itx.Script.Range(73, LENGTH_OF_AMOUNT).AsBigInteger();
+                    toB = itx.Script.Range(82, LENGTH_OF_SCRIPTHASH);
+                    fromB = itx.Script.Range(103, LENGTH_OF_SCRIPTHASH);
                 }
-                else if (tokenBR == itx.Script.Range(63,20) && tokenSR == itx.Script.Range(146,20))
+                else if (tokenBR == itx.Script.Range(52, LENGTH_OF_SCRIPTHASH) && tokenSR == itx.Script.Range(124, LENGTH_OF_SCRIPTHASH))
                 {
                     amountB = itx.Script.Range(1, LENGTH_OF_AMOUNT).AsBigInteger();
                     toB = itx.Script.Range(10, LENGTH_OF_SCRIPTHASH);
                     fromB = itx.Script.Range(31, LENGTH_OF_SCRIPTHASH);
                     
-                    amountS = itx.Script.Range(84, LENGTH_OF_AMOUNT).AsBigInteger();
-                    toS = itx.Script.Range(93, LENGTH_OF_SCRIPTHASH);
-                    fromS = itx.Script.Range(114, LENGTH_OF_SCRIPTHASH);
+                    amountS = itx.Script.Range(73, LENGTH_OF_AMOUNT).AsBigInteger();
+                    toS = itx.Script.Range(82, LENGTH_OF_SCRIPTHASH);
+                    fromS = itx.Script.Range(103, LENGTH_OF_SCRIPTHASH);
                 }
                 else
                 {
@@ -95,6 +97,8 @@ namespace Neo.SmartContract
             {
                 return false;
             }
+
+           	return false; 
         }
     }
 }
